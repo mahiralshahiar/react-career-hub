dbf80a3b44ff6614fbbd8625e9f17f6f416837f3
Mahir Al Shariar <mrmahiralshariar9@gmail.com>
2023-10-04 03:55:23 +0600

    Added Show All job Conditional Btn

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,3 +1,6 @@
+import { CiLocationOn } from 'react-icons/ci';
+import { AiOutlineDollarCircle } from 'react-icons/ai';
+
 
 const Job = ({ job }) => {
     const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
@@ -10,6 +13,10 @@ const Job = ({ job }) => {
                 <div>
                     <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] text-[#7E90FE] mr-4">{remote_or_onsite}</button>
                     <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] text-[#7E90FE] mr-4">{job_type}</button>
+                </div>
+                <div className='mt-4 flex '>
+                    <h2 className='flex items-end mr-4'><CiLocationOn className='text-2xl'></CiLocationOn>{location}</h2>
+                    <h2 className=' flex items-end'> <AiOutlineDollarCircle className='text-2xl'></AiOutlineDollarCircle>{salary}</h2>
                 </div>
                 <div className="card-actions ">
                     <button className="btn text-white bg-[#7E90FE] flex">View Details</button>
